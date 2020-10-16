b731a387829cd9878f5a2ccab52b48e95d168405
Shubham Rana <ranashubham7296@gmail.com>
2020-10-16 18:14:03 +0530

    Welcome to address book system

---
+++ AddressBookSystem.java
@@ -0,0 +1,10 @@
+package AddressBookSystem;
+
+public class AddressBookSystem {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to address book system program");
+	}
+
+}
